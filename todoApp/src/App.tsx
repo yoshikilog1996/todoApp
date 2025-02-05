414b95b0509a409e26056027bce051fd917af023
bando miki <miki@MacBook-Air.local>
2025-02-05 15:57:19 +0900

    ステートフックidを削除し、グローバル変数でid管理する処理に修正しました

--- todoApp/src/App.tsx
+++ todoApp/src/App.tsx
@@ -3,22 +3,21 @@ import { Todo } from './types'
 import AddTodo from './AddTodo'
 
 const initialTodos: Todo[] = []
+let id = 0
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>(initialTodos)
-  const [id, setId] = useState(0)
 
   function handleAddTodo(title: string) {
     if (title) {
       setTodos(prevTodos => [
         ...prevTodos,
         {
-          id: id + 1,
+          id: ++id,
           title,
           isDone: false
         }
       ])
-      setId(id + 1)
     }
   }
 
